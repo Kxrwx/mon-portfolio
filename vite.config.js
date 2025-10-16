0b47805065a13844eba66c10722befa1d7f332ad
Kxrwx <maxime.iffland@icloud.com>
2025-10-16 18:37:49 +0200

    v0

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mon-portfolio/', // 👈 nom de ton dépôt GitHub
 })
