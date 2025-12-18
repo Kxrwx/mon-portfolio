7cc40684dc9ffa79a3f89d561121cbe47bef480e
Kxrwx <maxime.iffland@icloud.com>
2025-12-18 14:04:29 +0100

    Changement de border et suppression de projet

--- src/components/project/ProjetPortfolio1.jsx
+++ src/components/project/ProjetPortfolio1.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import '../../styles/project/ProjetPortfolio1.css'
-import TimelinePortfolio1 from './ProjetPortfolio1/TimelinePortfolio1'
-import TechPortfolio1 from './ProjetPortfolio1/TechPortfolio1'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export default function ProjectPortfolio1() {
   return (
     <div className='project-content-main'>
         <h1>Mon portfolio 1</h1>
-        <TimelinePortfolio1/>
-        <TechPortfolio1/>
+    
     </div>
   )
 }
--- src/components/project/ProjetPortfolio2.jsx
+++ src/components/project/ProjetPortfolio2.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import '../../styles/project/ProjetPortfolio2.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export default function ProjectPortfolio2() {
   return (
